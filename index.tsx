31faf3297e1296944a613ae290acd83f30977d37
tmaihoff <tobiasmaihoff@gmail.com>
2025-12-06 22:36:54 +0100

    feat: integrate Vercel Analytics for enhanced tracking and insights

--- index.tsx
+++ index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { App } from "./src/components/App";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
